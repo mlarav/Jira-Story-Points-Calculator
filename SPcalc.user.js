1d497f18d42d2c00fc54486856d11d0db9f53268
mlarav <58264067+mlarav@users.noreply.github.com>
2021-05-13 00:43:23 -0300

    Create SPcalc.user.js
    This Tampermonkey Script works to show a modal with the Total of sprints Story Points and the total remaining. It works perfectly with Jira Server 8.11.0

---
+++ SPcalc.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name        SPcalc
+// @namespace   jiraSPCalc
+// @description Jira storypoint calculator
+// @include     https://your.jira.site.com/RapidBoard.jspa*
+// @version     1.0
+// @grant       none
+// @author   mlarav83@gmail.com
+// ==/UserScript==
+var updateSprintMetaBox = function(){
+
+ // Targets config
+ var spItems = $('body').find('aui-badge[title="Story Points"]'); //All story points
+ var spDone = $('.ghx-column[data-column-id="571"]').find('aui-badge[title="Story Points"]');
+ var spMetaBox = $('body').find('.ghx-view-section'); //Sprint meta box on page header
+
+ // Clean previous SPMetas
+ $(spMetaBox).find('span.story-points-metabox').remove();
+
+//Sum the SP
+ var spCount = $(spItems).length;
+ var spSum = 0;
+ var spSumDone = 0;
+ $.each(spItems, function(key, item){
+ spSum += parseInt($(item).text()) || 0;
+ })
+  $.each(spDone, function(key, item){
+ spSumDone += parseInt($(item).text()) || 0;
+ })
+
+ //console.table({spCount, spSum});
+
+ // Add SP meta message to page header
+ $(spMetaBox).prepend(`
+ <span class="story-points-metabox">
+ <span class="aui-icon aui-icon-small aui-iconfont-vid-full-connection-circle">Story Points meta</span>
+ Total: ${spSum}, Remain: ${(spSum - spSumDone)}
+ </span>
+ `);
+}
+
+// On page load
+setTimeout(updateSprintMetaBox, 2000);
+
+//Listener
+
+$('body').on('DOMSubtreeModified', ".subnav-container", function(){
+   $(document).ready(function(){
+    this.updateSprintMetaBox();
+});
+ setTimeout(updateSprintMetaBox, 2000);
+});
+
+    
